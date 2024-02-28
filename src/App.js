1c1c171e7c7a30517ffb73c01b2ff4adac23af50
Adil Rehman <adilg5625@gmail.com>
2024-02-28 17:43:15 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,109 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import moonIcon from './assets/moon.png';
+import sunIcon from './assets/sun.png';
 import './App.css';
+import Header from "./Components/Header";
+import Keypad from "./Components/Keypad";
+
+
+const usedKeyCodes = [
+  48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102,
+  103, 104, 105, 8, 13, 190, 187, 189, 191, 56, 111, 106, 107, 109,
+];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const operators = ["-", "+", "/", "*"];
 
 function App() {
+  const [IsDarkMode, setIsDarkMode] = useState(JSON.parse(localStorage.getItem("calculator-app-mode")) || false);
+  const [expression, setExpression] = useState("");
+  const [result, setResult] = useState("");
+  const [history, setHistory] = useState(JSON.parse(localStorage.getItem("calculator-app-history")) || []);
+
+  const handleKeyPress =(keyCode,key)=>{
+    if(!keyCode)return
+    if(!usedKeyCodes.includes(keyCode)) return;
+
+    if(numbers.includes(key)){
+      if(key==="0"){
+        if(expression.length===0)return;
+      }
+      calculateResult(expression + key);
+      setExpression(expression+key)
+    }
+    else if(operators.includes(key)){
+      if(!expression)return;
+
+      const lastChar=expression.slice(-1);
+      if(operators.includes(lastChar))return;
+      if(lastChar===".")return;
+
+      setExpression(expression + key);
+    } else if(key === "."){
+      if(!expression)return;
+      const lastChar=expression.slice(-1)
+      if(!numbers.includes(lastChar))return;
+
+      setExpression(expression + key);
+    } else if(keyCode===8){
+      if(!expression)return;
+      calculateResult(expression.slice(0,-1));
+      setExpression(expression.slice(0,-1));
+    } else if(keyCode===13){
+      if(!expression)return;
+      calculateResult(expression);
+
+      const tempHistory = [...history]
+      if(history.length>20) tempHistory = tempHistory.slice(0,1);
+      tempHistory.push(expression);
+      setHistory(tempHistory);
+    }
+  };
+
+  const calculateResult = (exp) => {
+    if(!exp) {
+      setResult("");
+      return;
+    }
+    const lastChar=exp.slice(-1);
+    if(!numbers.includes(lastChar))exp=exp.slice(0,-1)
+
+    try {
+    const answer = new Function('return ' + exp)();
+    setResult(answer);
+  } catch (error) {
+    setResult("Error");
+  }
+  }
+
+
+  useEffect(()=>{
+    localStorage.setItem("calculator-app-mode", JSON.stringify(IsDarkMode))
+  },[IsDarkMode])
+
+  useEffect(()=>{
+    localStorage.setItem("calculator-app-history", JSON.stringify(history))
+  },[history])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+    <div className="app" 
+    tabIndex="0"
+    onKeyDown={(event)=> handleKeyPress(event.keyCode, event.key)}
+     data-theme={IsDarkMode ? "dark" : ""}>
+      <div className="app_calculator">
+        <div className="app_calculator_navbar">
+          <div className="app_calculator_navbar_toggle"
+            onClick={()=>setIsDarkMode(!IsDarkMode)}
+          >
+            <div className={`app_calculator_navbar_toggle_circle ${IsDarkMode? "app_calculator_navbar_toggle_circle_active":""}`}></div>
+          </div>
+          <img src={IsDarkMode ? moonIcon : sunIcon} alt="mode"/>
+        </div>
+          <Header expression={expression} result={result} history={history}/>
+          <Keypad handleKeyPress={handleKeyPress}/>
+      </div>
     </div>
+   </>
   );
 }
 
